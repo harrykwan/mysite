c257be99a2192f1935b63f4c760ee525c2e37020
harrykwan <33515754+harrykwan@users.noreply.github.com>
2018-08-31 22:33:00 +0900

    Create tsefei.js

---
+++ tsefei.js
@@ -0,0 +1,6 @@
+var feild1 = document.getElementsByName("sess")[0];
+var feild2 = document.getElementsByName("subject")[0];
+feild2.value = "PMATH";
+var feild3 = document.getElementsByName("cournum")[0];
+feild3.value = 351;
+document.getElementsByTagName("form")[0].submit();
