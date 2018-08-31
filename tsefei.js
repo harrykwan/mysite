8f1d49e7687a3958316508ede234088e2b00ae24
harrykwan <33515754+harrykwan@users.noreply.github.com>
2018-08-31 22:40:41 +0900

    Update tsefei.js

--- tsefei.js
+++ tsefei.js
@@ -4,3 +4,11 @@ feild2.value = "PMATH";
 var feild3 = document.getElementsByName("cournum")[0];
 feild3.value = 351;
 document.getElementsByTagName("form")[0].submit();
+
+//---------------------------------------------------------------nextpage
+var checking = setInterval(function(){
+	var cotaleft = parseInt(document.getElementsByTagName("td")[12].innerText)-parseInt(document.getElementsByTagName("td")[13].innerText);
+	if (cotaleft>0){
+		alert('wow, someone drop');
+	}
+},60000);
